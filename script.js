99a98df5e54099137af182099d7fe82e4c8c4661
RameshwariShinde <168254371+RameshwariShinde@users.noreply.github.com>
2025-04-13 17:50:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+{
+    "folders": [
+      {
+        "path": "."
+      }
+    ],
+    "settings": {
+      "files.exclude": {
+        "**/.git": true,
+        "**/.DS_Store": true
+      },
+      "editor.tabSize": 2
+    }
+  }
+  
